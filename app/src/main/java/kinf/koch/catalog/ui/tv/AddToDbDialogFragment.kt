713bcb13a03f6d3e6b70c73e06c7d79a5a4936f1
David <david.koch@michael-mueller-verlag.de>
2020-08-06 23:48:19 +0200

    added dialog for rating

--- app/src/main/java/kinf/koch/catalog/ui/tv/AddToDbDialogFragment.kt
+++ app/src/main/java/kinf/koch/catalog/ui/tv/AddToDbDialogFragment.kt
@@ -3,27 +3,59 @@ package kinf.koch.catalog.ui.tv
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.DialogInterface
+import android.media.Image
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import androidx.core.view.forEach
+import androidx.core.view.forEachIndexed
 import androidx.fragment.app.DialogFragment
 import io.reactivex.internal.subscriptions.SubscriptionHelper.cancel
 import kinf.koch.catalog.R
+import kotlinx.android.synthetic.main.dialog_add_to_db.*
+import kotlinx.android.synthetic.main.dialog_add_to_db.view.*
 
-class AddToDbDialogFragment: DialogFragment() {
+class AddToDbDialogFragment : DialogFragment() {
+
+    var numberOfStars = 0
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return activity?.let {
+        val dialog = activity?.let {
             // Use the Builder class for convenient dialog construction
             val builder = AlertDialog.Builder(it)
-            builder.setMessage("Hello?")
-                .setPositiveButton("Yes",
-                    DialogInterface.OnClickListener { dialog, id ->
-                        // FIRE ZE MISSILES!
-                    })
-                .setNegativeButton("no",
-                    DialogInterface.OnClickListener { dialog, id ->
-                        // User cancelled the dialog
-                    })
-            // Create the AlertDialog object and return it
+            val inflater = requireActivity().layoutInflater
+            val view = inflater.inflate(R.layout.dialog_add_to_db, null)
+
+            val stars = mutableListOf<ImageView>()
+            view.starsLayout.forEachIndexed { indexOuter, star ->
+                star.setOnClickListener {
+                    numberOfStars = 0
+                    stars.forEachIndexed { indexInner, star ->
+                        if (indexInner <= indexOuter) {
+                            star.setImageResource(R.drawable.ic_baseline_star_24)
+                            numberOfStars++
+                        } else {
+                            star.setImageResource(R.drawable.ic_baseline_star_border_24)
+                            numberOfStars--
+                        }
+                    }
+
+                }
+                stars.add(star as ImageView)
+            }
+
+            builder
+                .setView(view)
+                .setPositiveButton("Save", { dialog, id ->
+                    Log.i("hallo", numberOfStars.toString())
+                    // FIRE ZE MISSILES!
+                })
+                .setNegativeButton("Cancel", { dialog, id ->
+                    // User cancelled the dialog
+                })
+            // Create the AlertDialog object
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
+        return dialog
     }
 }
--- app/src/main/java/kinf/koch/catalog/ui/tv/SearchFragment.kt
+++ app/src/main/java/kinf/koch/catalog/ui/tv/SearchFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -35,7 +36,6 @@ class SearchFragment : Fragment() {
         recyclerView = rootView.findViewById(R.id.recyclerView)
         clickListener = object : OnClickListener {
             override fun onCheckBoxClick(item: EitherMovieOrSeries) {
-                println("HALLOO")
               //  findNavController().navigate(R.id.action_searchTV_to_addToDbDialogFragment)
                 val dialog = AddToDbDialogFragment()
                 dialog.show(fragmentManager!!, "dialog")
